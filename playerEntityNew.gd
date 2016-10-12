bf42b5d36e142aac5785588b966228a6452d74b5
Henrique Campos <henriiquecampos@gmail.com>
2016-10-12 18:38:17 -0300

    Added main behaviours

---
+++ playerEntityNew.gd
@@ -0,0 +1,43 @@
+extends Node
+
+onready var head = get_node("head")
+onready var tail = get_node("tail")
+
+export var threshold = 0
+export (String) var left
+export (String) var right
+export (Color) var color
+var coll = preload("res://coll.tscn")
+var points = []
+
+func _ready():
+	set_fixed_process(true)
+	points.append(tail.get_pos())
+	points.append(head.get_pos())
+
+func _fixed_process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().set_pause(true)
+	head.translate(Vector2(0,-threshold * delta).rotated(head.get_rot()))
+	if Input.is_action_pressed(right):
+		head.rotate(-3 * delta)
+	elif Input.is_action_pressed(left):
+		head.rotate(3 * delta)
+	if head.get_pos().distance_to(points[points.size() -1]) > 10:
+		for child in get_children():
+			if child extends CollisionShape2D:
+				self.remove_child(child)
+		update()
+func _draw():
+	var c
+	var line
+
+	for i in range(0,points.size() -1):
+		draw_line(points[i], points[i + 1], color, 10)
+		c = coll.instance()
+		line = SegmentShape2D.new()
+		line.set_a(points[i])
+		line.set_b(points[i + 1])
+		self.add_child(c)
+		c.set_shape(line)
+	points.append(head.get_pos())
